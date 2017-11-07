7ce06c44f854173e7b3b187ed3842156153b70ce
V <smallbadoncelaugh@gmail.com>
2017-11-07 14:12:10 +0800

    the second update project.

--- src/main/java/com/binggol/Application.java
+++ src/main/java/com/binggol/Application.java
@@ -3,9 +3,8 @@ package com.binggol;
 
 
 
-@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        //SpringApplication.run(Application.class, args);
     }
 }
